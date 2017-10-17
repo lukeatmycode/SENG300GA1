f76cf940551d43d0b953eb5ee660f88f48e859f5
lukeatmycode <luke.kissick1@ucalgary.ca>
2017-10-17 12:29:43 -0600

    First try for implementing the listener

--- ca.ucalgary.seng300.a1/src/ca/ucalgary/seng300/a1/CoinSlotListenerDevice.java
+++ ca.ucalgary.seng300.a1/src/ca/ucalgary/seng300/a1/CoinSlotListenerDevice.java
@@ -8,21 +8,45 @@ import org.lsmr.vending.hardware.CoinSlotListener;
 
 public class CoinSlotListenerDevice implements CoinSlotListener {
 
+	private int value = 0;
+	private boolean disabled = false;
+	
+	public CoinSlotListenerDevice(CoinSlot slot) {
+		slot.register(this);
+		disabled = slot.isDisabled();
+		value = 0;	
+	}
+	/**
+     * Announces that the indicated hardware has been enabled.
+     * 
+     * @param hardware
+     *            The device that has been enabled.
+     */
 	@Override
 	public void enabled(AbstractHardware<? extends AbstractHardwareListener> hardware) {
-		// TODO Auto-generated method stub
+		hardware.enable();
+		hardware.notify();
 		
 	}
 
+	/**
+     * Announces that the indicated hardware has been disabled.
+     * 
+     * @param hardware
+     *            The device that has been enabled.
+     */
 	@Override
 	public void disabled(AbstractHardware<? extends AbstractHardwareListener> hardware) {
-		// TODO Auto-generated method stub
+		hardware.disable();
+		hardware.notify();
 		
 	}
 
 	@Override
 	public void validCoinInserted(CoinSlot slot, Coin coin) {
-		// TODO Auto-generated method stub
+		if (!disabled) {
+			value = value + coin.getValue();
+		}
 		
 	}
 
