faa04c2fe9a48b143ea112435b0742bdc0760c7c
stephenpollo <32685137+stephenpollo@users.noreply.github.com>
2017-10-19 21:39:50 -0600

    Create ButtonListenerDevice.java

---
+++ ca.ucalgary.seng300.a1/src/ca/ucalgary/seng300/a1/ButtonListenerDevice.java
@@ -0,0 +1,26 @@
+package ca.ucalgary.seng300.a1;
+
+import org.lsmr.vending.*;
+import org.lsmr.vending.hardware.*;
+
+public class ButtonListenerDevice implements SelectionButtonListener {
+
+	@Override
+	public void enabled(AbstractHardware<? extends AbstractHardwareListener> hardware) {
+		// do nothing for now
+		
+	}
+
+	@Override
+	public void disabled(AbstractHardware<? extends AbstractHardwareListener> hardware) {
+		// do nothing for now
+		
+	}
+
+	@Override
+	public void pressed(SelectionButton button) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
