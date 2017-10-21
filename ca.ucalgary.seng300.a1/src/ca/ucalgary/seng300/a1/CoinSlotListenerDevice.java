08b95a9cf301f2e8dafc8a3e2fa57890a24e45f8
Stephen <stephen.pollo@ucalgary.ca>
2017-10-20 18:17:39 -0600

    Merged the CoinSlotListenerDevice and ButtonListenerDevice into one class
    This makes it easier to check if there is enough money when deciding if
    pop should be dispensed. Also added a constructor that registers the
    listener with all relevant vending machine components automatically with
    only the vending machine reference. We could rename this class to just
    "ListenerDevice" ?

--- ca.ucalgary.seng300.a1/src/ca/ucalgary/seng300/a1/CoinSlotListenerDevice.java
+++ ca.ucalgary.seng300.a1/src/ca/ucalgary/seng300/a1/CoinSlotListenerDevice.java
@@ -1,18 +1,34 @@
 package ca.ucalgary.seng300.a1;
 
-import org.lsmr.vending.Coin;
-import org.lsmr.vending.hardware.AbstractHardware;
-import org.lsmr.vending.hardware.AbstractHardwareListener;
-import org.lsmr.vending.hardware.CoinSlot;
-import org.lsmr.vending.hardware.CoinSlotListener;
+import org.lsmr.vending.*;
+import org.lsmr.vending.hardware.*;
 
-public class CoinSlotListenerDevice implements CoinSlotListener {
+public class CoinSlotListenerDevice implements CoinSlotListener, SelectionButtonListener {
 
 	private int value = 0;
+	private VendingMachine vending;
 	
 	public CoinSlotListenerDevice(CoinSlot slot) {
 		slot.register(this);
 		value = 0;	
+	}
+	
+	public CoinSlotListenerDevice(SelectionButton button) {
+		button.register(this);
+	}
+	
+	/**
+	 * Main constructor. Registers the listener to the coin slot and all 
+	 * buttons of the vending machine
+	 * @param vend the VendingMachine object to listen to
+	 */
+	public CoinSlotListenerDevice(VendingMachine vend) {
+		this.vending = vend;
+		for (int i = 0; i < vend.getNumberOfSelectionButtons(); i++) {
+			vend.getSelectionButton(i).register(this);
+		}
+		vend.getCoinSlot().register(this);
+		value = 0;
 	}
 	
 	/**
@@ -72,6 +88,37 @@ public class CoinSlotListenerDevice implements CoinSlotListener {
 		else {
 			// TODO could create a custom exception or just have it do nothing. Thoughts?
 		}
+	}
+
+	@Override
+	public void pressed(SelectionButton button) {
+		// TODO Auto-generated method stub
+		int index = findButtonIndex(button);
+		int cost = vending.getPopKindCost(index);
+		
+		if (value >= cost) {
+			payForItem(cost);
+			//dispense the pop and deduct the cost from value
+		}
+		else {
+			// Probably print error message to display in future assignment?
+		}
+		
+	}
+	
+	/**
+	 * Finds the index of a SelectionButton in this objects VendingMachine reference's
+	 * button array
+	 * @param button the SelectionButton to search for in the VendingMachine button array
+	 * @return the index of the SelectionButton in the button array, -1 if it's not there
+	 */
+	public int findButtonIndex (SelectionButton button) {
+		for (int i = 0; i < vending.getNumberOfSelectionButtons(); i++) {
+			if (vending.getSelectionButton(i) == button) {
+				return i;
+			}
+		}
+		return -1;
 	}
 	
 }
