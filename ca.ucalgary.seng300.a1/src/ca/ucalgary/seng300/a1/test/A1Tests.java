483403881b5bc227ace2dd884441b612278f1365
cynthiaxw <365683780@qq.com>
2017-10-22 13:52:24 -0600

    Update A1Tests.java
    first thought of the test case

--- ca.ucalgary.seng300.a1/src/ca/ucalgary/seng300/a1/test/A1Tests.java
+++ ca.ucalgary.seng300.a1/src/ca/ucalgary/seng300/a1/test/A1Tests.java
@@ -1,5 +1,8 @@
 package ca.ucalgary.seng300.a1.test;
 
+import org.lsmr.vending.*;
+import ca.ucalgary.seng300.a1.*;
+import org.lsmr.vending.hardware.*;
 import static org.junit.Assert.*;
 
 import org.junit.After;
@@ -8,27 +11,42 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class A1Tests {
-	//placeholder class for package creation. Delete once there is a real test made. 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
+/**
+ * Unit tests the CoinSlotlistenerDevice
+ */
+public class A1Tests {
+	private CoinSlot slot;
+	private SelectionButton button;
+	private VendingMachine vend;
+	private CoinSlotListenerDevice listener1, listener2, listener3, listener4, listener5, listener6;
 
 	@Before
 	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
+		slot = new CoinSlot(new int[] {1});
+		listener1 = new CoinSlotListenerDevice(slot);
+		listener2 = new CoinSlotListenerDevice(slot);
+		listener3 = new CoinSlotListenerDevice(button);
+		listener4 = new CoinSlotListenerDevice(button);
+		listener5 = new CoinSlotListenerDevice(vend);
+		listener6 = new CoinSlotListenerDevice(vend);
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetValue() throws DisabledException {
+		slot.addCoin(new Coin(1));
+		assertEquals(1,listener1.getValue());
+		assertEquals(1,listener1.getValue());
+	}
+	
+	@Test
+	public void testPressButtonWhenCoinsEnough() {
+		
+	}
+	
+	@Test
+	public void testPressButtonWhenCoinsNotEnough() {
+		
 	}
 
 }
